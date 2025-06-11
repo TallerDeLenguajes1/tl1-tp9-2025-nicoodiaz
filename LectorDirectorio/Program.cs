0572299d2c4e5a09a6a914439b705857895f4b58
DiazRobertoNicolas <nicolas0405.nd@gmail.com>
2025-06-11 04:34:44 +0000

    Ejercicio 1 terminado

--- LectorDirectorio/Program.cs
+++ LectorDirectorio/Program.cs
@@ -30,11 +30,22 @@ foreach (string directorios in nombreDirectorios)
     Console.WriteLine($"El nombre del directorio es: {infoDirectorios.Name}");
 }
 
+//Creo la lista de string para poder guardar la info de cada archivo y despues escribir esto en el CSV
+List<string> lineasParaCSV = new List<string>();
+//Añado esta linea, como para poder tener una referencia de que es cada columna
+lineasParaCSV.Add("Nombre del archivo; Tamanio en KB; Fecha ult. modificacion");
+
 //Para poder acceder a la info de los archivos
 string[] nombreArchivos = Directory.GetFiles(rutaDirectorio);
 foreach (string archivos in nombreArchivos)
 {
     FileInfo infoArchivos = new FileInfo(archivos);
     Console.WriteLine($"El nombre del archivo es: {infoArchivos.Name} y su tamanio en KB: {infoArchivos.Length / 1024}");
+    //Añado las lineas a la lista dependiendo de cuantos archivos tenga el directorio en el que estoy parado
+    lineasParaCSV.Add($"{infoArchivos.Name};{infoArchivos.Length / 1024};{infoArchivos.LastWriteTime}");
 }
 Console.ResetColor();
+//Creo la ruta del archivo
+string rutaCSV = @$"{rutaDirectorio}/reporte_archivos.csv";
+//Escribo en el archivo las lineas, pasandole la lista
+File.WriteAllLines(rutaCSV, lineasParaCSV);

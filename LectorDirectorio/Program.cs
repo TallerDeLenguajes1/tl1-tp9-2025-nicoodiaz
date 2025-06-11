e2f6bea0ada309cb6882071dcdfedc047df888ce
DiazRobertoNicolas <nicolas0405.nd@gmail.com>
2025-06-11 01:37:13 +0000

    Creacion ejercicio 1 y logica para poder acceder al directorio

---
+++ LectorDirectorio/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine("--------Analizador de directorios-------");
+Console.ResetColor();
+Console.WriteLine("Ingrese la ruta de un directorio: "); //Pedimos la ruta
+
+string? rutaDirectorio = Console.ReadLine();
+rutaDirectorio = @"/mnt/c/FACULTAD"; //Esta es una ruta para poder acceder a los archivos de Windows desde WSL-Ubuntu
+
+//Ciclo while, por si la ruta no es correcta, poder pedirla hasta que si lo sea
+while (!Directory.Exists(rutaDirectorio))
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"El Path {rutaDirectorio} del directorio no es valido. Intente de nuevo.");
+    Console.ResetColor();
+    rutaDirectorio = Console.ReadLine();
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"El Path {rutaDirectorio}, funciona");
+Console.ResetColor();
+//Para poder acceder a la info de los directorios
+string[] nombreDirectorios = Directory.GetDirectories(rutaDirectorio);
+//Para recorrer los array creados
+foreach (string directorios in nombreDirectorios)
+{
+    //Una instancia para poder acceder a metodos 
+    FileInfo infoDirectorios = new FileInfo(directorios);
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine($"El nombre del directorio es: {infoDirectorios.Name}");
+}
+
+//Para poder acceder a la info de los archivos
+string[] nombreArchivos = Directory.GetFiles(rutaDirectorio);
+foreach (string archivos in nombreArchivos)
+{
+    FileInfo infoArchivos = new FileInfo(archivos);
+    Console.WriteLine($"El nombre del archivo es: {infoArchivos.Name} y su tamanio en KB: {infoArchivos.Length / 1024}");
+}
+Console.ResetColor();
